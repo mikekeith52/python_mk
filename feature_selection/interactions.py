5494964042b056f2133a56619b6ddeb41fdae39c
Michael Keith <mikekeith52@gmail.com>
2020-04-21 13:57:40 -0400

    Bug fixes

--- feature_selection/interactions.py
+++ feature_selection/interactions.py
@@ -1,24 +1,25 @@
 def interact_cols(X,which='all',drop=None):
-	""" given a pandas dataframe of predictor variables (X), choose columns to interact
-		by default, this will interact all columns, but you can change parameter which to a dictionary where key is one columns and value is other
-		the column name will be the two original column names separated by a *
-		you can perform a triple interaction by ordering the dictionary correctly (columns will be placed in order they appear in dictionary)
-		you can then pass the interactions through the rfe module to whittle down predictors and arrive at the most powerful model
-		drop is a list of interactions to drop after the interactions have been run (example: you want to drop only a copule interacted columns and use which = "all" instead of writing a really long dictionary)
-		drop must be a list or str type
-	"""
+    """ given a pandas dataframe of predictor variables (X), choose columns to interact
+        by default, this will interact all columns, but you can change parameter which to a dictionary where key is one columns and value is other
+        the column name will be the two original column names separated by a *
+        you can perform a triple interaction by ordering the dictionary correctly (columns will be placed in order they appear in dictionary)
+        you can then pass the interactions through the rfe module to whittle down predictors and arrive at the most powerful model
+        drop is a list of interactions to drop after the interactions have been run (example: you want to drop only a copule interacted columns and use which = "all" instead of writing a really long dictionary)
+        drop must be a list or str type
+    """
 
-	if which == 'all':
-		for i, col in enumerate(X.columns):
-			i+=1
-			while i < len(X.columns):
-				X[f'{col}*{X.columns[i]}'] = X[col]*X[X.columns[i]]
-				i+=1
-	elif isinstance(which,dict):
-		for key, val in which.items():
-			X[f'{key}*{val}'] = X[{key}]*X[val]
-	else:
-		raise TypeError('which should be "all" or a dictionary of columns in X to interact')
+    if which == 'all':
+        first_len = len(X.columns)
+        for i, col in enumerate(X.columns):
+            i+=1
+            while i < first_len:
+                X[f'{col}*{X.columns[i]}'] = X[col]*X[X.columns[i]]
+                i+=1
+    elif isinstance(which,list):
+        for t in which:
+            X[f'{t[0]}*{t[1]}'] = X[t[0]]*X[t[1]]
+    else:
+        raise TypeError('which should be "all" or a list of tuples')
 
-	if isinstance(drop,(str,list)):
-		X.drop(columns=drop,inplace=True)
+    if isinstance(drop,(str,list)):
+        X.drop(columns=drop,inplace=True)
--- feature_selection/rfe.py
+++ feature_selection/rfe.py
@@ -1,43 +1,43 @@
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression
 
-def rfe(X,y,model_type="logistic",keep='auto',print_support=True,print_rankings=False):
+def rfe(X,y,model_type="logistic",keep='auto',print_support=False,print_rankings=False):
     """ recursive feature elimination
         from the sklearn documentation: 
-	Given an external estimator that assigns weights to features (e.g., the coefficients of a linear model), the goal of recursive feature elimination (RFE) is to select features by recursively considering smaller and smaller sets of features.
-	First, the estimator is trained on the initial set of features and the importance of each feature is obtained either through a coef_ attribute or through a feature_importances_ attribute. 
-	Then, the least important features are pruned from current set of features. That procedure is recursively repeated on the pruned set until the desired number of features to select is eventually reached.
-	X and y should be your predictors and dependent variable in pandas dataframe format.
-	Returns the reduced set of columns to use in your model.
+        Given an external estimator that assigns weights to features (e.g., the coefficients of a linear model), the goal of recursive feature elimination (RFE) is to select features by recursively considering smaller and smaller sets of features.
+        First, the estimator is trained on the initial set of features and the importance of each feature is obtained either through a coef_ attribute or through a feature_importances_ attribute. 
+        Then, the least important features are pruned from current set of features. That procedure is recursively repeated on the pruned set until the desired number of features to select is eventually reached.
+        X and y should be your predictors and dependent variable in pandas dataframe format.
+        Returns the reduced set of columns to use in your model.
         model_type supports the following values: 'logistic' or 'linear'
         keep will take 'auto' or a positive integer value -- if an integer, this will return the top however many predictors that are best according to RFE
     """
 
     if model_type == 'logistic':
-	   model = LogisticRegression()
+        model = LogisticRegression()
     elif model_type == 'linear':
         model = LinearRegression()
     else:
-        rasie TypeError("don't know which model to pick!")
+        raise TypeError("don't know which model to pick!")
 
-	rfe = RFE(model)
-	rfe = rfe.fit(X, y.values.ravel())
+    rfe = RFE(model)
+    rfe = rfe.fit(X, y.values.ravel())
 
-	# should we keep each column according to the rfe?
-	if print_support:
-			for i, col in enumerate(X.columns):
-			    print({col:rfe.support_[i]})
+    # should we keep each column according to the rfe?
+    if print_support:
+            for i, col in enumerate(X.columns):
+                print({col:rfe.support_[i]})
 
     # what is each column's rank according to the RFE?
     if print_rankings:
         for i, col in enumerate(X.columns):
             print({col:rfe.ranking_[i]})
-            
+
     # create a list of columns to use in model
     if keep == 'auto':
-        rfe_cols = [X.columns[i] if e for i,e in enumerate(rfe.support_)]
+        rfe_cols = [X.columns[i] for i, e in enumerate(rfe.support_) if e]
     elif isinstance(keep,int) & (keep > 0):
-        rfe_cols = [X.columns[i] if e <= keep for i,e in enumerate(rfe.ranking_)]
+        rfe_cols = [X.columns[i] for i,e in enumerate(rfe.ranking_) if e <= keep]
     else:
         raise TypeError('keep must be "auto" or a positive integer')
 
